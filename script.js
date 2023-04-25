aa52bc1d694cae5c86007734ac071cff1cf2c91c
RakocziL <levente.rakoczi@gmail.com>
2023-04-25 13:08:02 +0200

    PopUp AddBook Window created

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+/* Test Button */
+const testButton = document.getElementById("test");
+testButton.addEventListener("click", ()=>{
+    myLibrary.forEach(books => {
+        console.log(books.title)
+        console.log(books.read)
+    });
+})
+/* PopUp Window events */
+const openPopupButton = document.getElementById('open-popup');
+const closePopupButton = document.getElementById('close-popup');
+const popupContainer = document.getElementById('popup-container');
+
+openPopupButton.addEventListener('click', () => {
+  popupContainer.style.display = 'block';
+});
+
+closePopupButton.addEventListener('click', () => {
+  popupContainer.style.display = 'none';
+});
+
+popupContainer.addEventListener('click', (event) => {
+  if (event.target === popupContainer) {
+    popupContainer.style.display = 'none';
+  }
+});
+
+/* Base Library Function */
 let myLibrary = []
 
 function book(title, author, pages, read){
@@ -21,30 +49,17 @@ addToLibrary(theFat)
 addToLibrary(theMan)
 
 
-myLibrary.forEach(books => {
-    console.log(books.title)
-    console.log(books.read)
+/* Creating a New Book with PopUp Window */
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
+const bookRead = document.getElementById("read");
+
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", (event) =>{
+    event.preventDefault();
+    const newBook = new book(bookTitle.value,bookAuthor.value,bookPages.value,bookRead.value ==1? "Read":"Not Read");
+    addToLibrary(newBook);
 });
 
-
-
-
-
-
-const openPopupButton = document.getElementById('open-popup');
-const closePopupButton = document.getElementById('close-popup');
-const popupContainer = document.getElementById('popup-container');
-
-openPopupButton.addEventListener('click', () => {
-  popupContainer.style.display = 'block';
-});
-
-closePopupButton.addEventListener('click', () => {
-  popupContainer.style.display = 'none';
-});
-
-popupContainer.addEventListener('click', (event) => {
-  if (event.target === popupContainer) {
-    popupContainer.style.display = 'none';
-  }
-});
